01293eb475181e4a7e528995d6e4cb582daaaf78
Your Name <you@example.com>
2022-04-27 21:25:48 -0600

    migracion tabla rol y usuario

---
+++ database/migrations/2022_04_28_023434_create_rols_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rol', function (Blueprint $table) {
+            $table->id(); // UNSIGNED BIG INTENTER, PRIMARY KEY, AUTO INCREMENT
+            $table->string('nombre', 50);
+            $table->integer('estado')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rol');
+    }
+};
---
+++ database/migrations/2022_04_28_031630_agregar_campos_rol_y_estad_usuario.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('estado')->default(1);
+            $table->unsignedBigInteger('id_rol');
+
+            $table->foreign('id_rol')->references('id')->on('rol');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['estado']);
+            $table->dropForeign(['id_rol']);
+            $table->dropColumn(['id_rol']);
+        });
+    }
+};
